4adb892bf25ee3531cd1366f57a7dc93a382c6f6
M K <sumukha.k@zeiss.com>
2023-11-21 01:10:27 +0530

    added basic skeleton and css for the taskflow categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-
+import "./app.css"
 function App() {
-
   return (
     <>
       <div>
         <h1>Task Flow</h1>
+        <div className="main-wrapper">
+        <div className="todo-wrapper">
+          <div className="todo-heading">To Do</div>
+          <div className="tasks-wrapper"></div>
+        </div>
+        <div className="inprogress-wrapper">
+          <div className="inprogress-heading">In Progress</div> 
+          <div className="tasks-wrapper"></div>
+        </div>
+        <div className="done-wrapper">
+          <div className="done-heading">Done</div>
+          <div className="tasks-wrapper"></div>
+        </div>
+      </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
